8677c436f2ee8810f1b20b399596bf4a0ba84cce
ldelia <ldelia@snappminds.com>
2013-04-22 15:14:53 -0300

    Se agregó al sitemap de ejemplo los nombres de cada nodo

--- src/Acme/DemoBundle/Sitemap/SitemapManager.php
+++ src/Acme/DemoBundle/Sitemap/SitemapManager.php
@@ -12,16 +12,16 @@ class SitemapManager extends AbstractSitemapManager
 {
     protected function buildSitemap()
     {
-        $sitemap = new Sitemap( "Home" );
+        $sitemap = new Sitemap( "H-1", "Home" );
         
-        $m1 = $sitemap->getRoot()->addChild( new Node("Gestión de Datos Maestros", new RouteBasedLocation("comprasRoute") ) );
-        $m1->addChild( new Node( "Productos", new RouteBasedLocation("comprasXFRoute") ) );
+        $m1 = $sitemap->getRoot()->addChild( new Node("GDM-1", "Gestión de Datos Maestros", new RouteBasedLocation("comprasRoute") ) );
+        $m1->addChild( new Node( "GDM-1-1","Productos", new RouteBasedLocation("comprasXFRoute") ) );
         
-        $m2 = $sitemap->getRoot()->addChild( new Node("Gestión de Compras", new RouteBasedLocation("_demo_hello", array( 'name' => 'colo' ) ) ));
-        $m2->addChild( new Node( "Pedido de Compras", new RouteBasedLocation("comprasXFRoute") ) );
+        $m2 = $sitemap->getRoot()->addChild( new Node("COMPRAS-1","Gestión de Compras", new RouteBasedLocation("_demo_hello", array( 'name' => 'colo' ) ) ));
+        $m2->addChild( new Node( "COMPRAS-1-1","Pedido de Compras", new RouteBasedLocation("comprasXFRoute") ) );
         
-        $m3 = $sitemap->getRoot()->addChild( new Node("Gestión de Ventas", null, false) );
-        $m3->addChild( new Node("Facturar", new RouteBasedLocation("comprasXFRoute") ) );
+        $m3 = $sitemap->getRoot()->addChild( new Node("VENTAS-1","Gestión de Ventas", null, false) );
+        $m3->addChild( new Node("VENTAS-1-1","Facturar", new RouteBasedLocation("comprasXFRoute") ) );
         return $sitemap;
     }    
 }
